db580ab1b1c270f7893300dc13ba5d6273bcfb35
Otix-x <phuoc.nx205232@sis.hust.edu.vn>
2023-11-16 20:48:56 +0700

    Add ProductTable component to Products page

---
+++ app/utils/categories.ts
@@ -0,0 +1,21 @@
+const categories = [
+    "Apple",
+    "Samsung",
+    "Xiaomi",
+    "Oppo",
+    "Realme",
+    "Nokia",
+    "Oneplus",
+    "Asus",
+    "Vivo",
+    "Nubia",
+    "Techno",
+    "Infinix",
+    "TCL",
+    "Redmi",
+    "Google",
+    "ZTE",
+  ];
+  
+  export default categories;
+  
